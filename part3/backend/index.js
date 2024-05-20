19f730621adb4a7e665f77a8304bc79d460a4084
Jack LaVelle <jacklavelle17@gmail.com>
2024-05-20 19:26:04 -0400

    Adds the part 3 directories for the backend (which contains the frontend production build named dist) and the frontend

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const requestLogger = (request, response, next) => {
     console.log("Method:", request.method);
@@ -11,6 +12,8 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger);
 app.use(express.json());
+app.use(cors());
+app.use(express.static("dist"));
 
 let notes = [
     {
@@ -80,7 +83,7 @@ app.post("/api/notes", (request, response) => {
     response.json(note);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
---
+++ part3/frontend/src/App.jsx
@@ -0,0 +1,114 @@
+import { useState, useEffect } from "react";
+import Note from "./components/Note";
+import noteService from "./services/notes";
+
+const ErrorNotification = ({ message }) => {
+    if (message === null) {
+        return null;
+    }
+
+    return <div className="error">{message}</div>;
+};
+
+const Footer = () => {
+    const footerStyle = {
+        color: "green",
+        fontStyle: "italic",
+        fontSize: 16,
+    };
+
+    return (
+        <div style={footerStyle}>
+            <br />
+            <em>
+                Note app, Department of Computer Science, University of Helsinki
+                2024
+            </em>
+        </div>
+    );
+};
+
+const App = () => {
+    const [notes, setNotes] = useState([]);
+    const [newNote, setNewNote] = useState("a new note...");
+    const [showAll, setShowAll] = useState(true);
+    const [errorMessage, setErrorMessage] = useState(null);
+
+    useEffect(() => {
+        noteService.getAll().then((initialNotes) => {
+            setNotes(initialNotes);
+        });
+    }, []);
+
+    const notesToShow = showAll
+        ? notes
+        : notes.filter((note) => note.important === true);
+
+    const addNote = (event) => {
+        event.preventDefault();
+        const noteObject = {
+            content: newNote,
+            important: Math.random() < 0.5,
+        };
+
+        noteService.create(noteObject).then((returnedNote) => {
+            notes.concat(returnedNote);
+            setNewNote("");
+        });
+    };
+
+    const toggleImportanceOf = (id) => {
+        const note = notes.find((note) => note.id === id);
+        const changedNote = { ...note, important: !note.important };
+
+        noteService
+            .update(id, changedNote)
+            .then((returnedNote) => {
+                setNotes(
+                    notes.map((note) => (note.id !== id ? note : returnedNote))
+                );
+            })
+            .catch((error) => {
+                setErrorMessage(
+                    `The following note: "${note.content}" was already removed from the server`
+                );
+                setTimeout(() => {
+                    setErrorMessage(null);
+                }, 5000);
+                setNotes(notes.filter((n) => n.id !== id));
+            });
+    };
+
+    const handleNoteChange = (event) => {
+        console.log(event.target.value);
+        setNewNote(event.target.value);
+    };
+
+    return (
+        <div>
+            <h1>Notes</h1>
+            <ErrorNotification message={errorMessage} />
+            <div>
+                <button onClick={() => setShowAll(!showAll)}>
+                    show {showAll ? "important" : "all"}
+                </button>
+            </div>
+            <ul>
+                {notesToShow.map((note) => (
+                    <Note
+                        key={note.id}
+                        note={note}
+                        toggleImportance={() => toggleImportanceOf(note.id)}
+                    />
+                ))}
+            </ul>
+            <form onSubmit={addNote}>
+                <input value={newNote} onChange={handleNoteChange} />
+                <button type="submit">save</button>
+            </form>
+            <Footer />
+        </div>
+    );
+};
+
+export default App;
