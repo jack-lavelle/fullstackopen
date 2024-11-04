686a662a938bda0f64239f5c09d017a1619f66cc
Jack LaVelle <jacklavelle17@gmail.com>
2024-11-03 22:41:52 -0500

    Finished up to the start of 4.1 - 4.2 exercises

--- notes/part3/backend/models/note.js
+++ notes/part3/backend/models/note.js
@@ -1,31 +1,20 @@
 const mongoose = require("mongoose");
 
-mongoose.set("strictQuery", false);
-
-const url = process.env.MONGODB_URI;
-
-console.log("Connecting to the MongoDB database at:", url);
-
-mongoose
-    .connect(url)
-    .then((result) => {
-        console.log("Connected to MongoDB.");
-    })
-    .catch((error) => {
-        console.log("Error connecting to MongoDB: ", error.message);
-    });
-
 const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
+  content: {
+    type: String,
+    minlength: 5,
+    required: true,
+  },
+  important: Boolean,
 });
 
 noteSchema.set("toJSON", {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
-    },
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
 });
 
 module.exports = mongoose.model("Note", noteSchema);
